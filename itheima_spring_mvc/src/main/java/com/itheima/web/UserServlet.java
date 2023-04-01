76eef1d7bad991d3537900aa74877acacc5f08c9
“hop” <hopeckel@gmail.com>
2023-04-01 15:47:26 -0400

    20230401 Init

--- itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
+++ itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,9 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ServletContext servletContext = req.getServletContext();
+        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
         UserService userService = app.getBean(UserService.class);
         userService.save();
     }
