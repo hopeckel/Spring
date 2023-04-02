e516102bba0b23c713bffed9158850211a714293
“hop” <hopeckel@gmail.com>
2023-04-01 23:08:51 -0400

    20230401 Init

--- itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
+++ itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
@@ -2,13 +2,34 @@ package com.itheima.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/quick")
     public String save() {
         System.out.println("Controller save running");
-        return "success.jsp";
+        return "success";
+    }
+
+    @RequestMapping("/quick2")
+    public ModelAndView save2() {
+        /**
+         *
+         */
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("username", "itcast");
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
+    @RequestMapping("/quick3")
+    public ModelAndView save3(ModelAndView modelAndView) {
+
+        modelAndView.addObject("username", "itcast");
+        modelAndView.setViewName("success");
+        return modelAndView;
     }
 }
